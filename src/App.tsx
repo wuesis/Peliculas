005ef8aafb6ee37a4f71bb239badce5dc46901b2
ITSVANALBERTO MORENO <itsvanalberto.moreno@technicolor.com>
2022-07-08 17:03:05 -0500

    React component example - virtual dom demostration with a reac component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [date,getDate] = useState(new Date());
+
+  useEffect(()=>{
+    const timerId = setTimeout( () => {
+      getDate(new Date())
+    },1000)
+
+    return () => clearInterval(timerId);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <input></input>
+       <p>{date.toString()}</p>
     </div>
   );
 }
