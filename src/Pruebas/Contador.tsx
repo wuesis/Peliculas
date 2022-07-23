e633dc85b5cef548b9fc4e9463d4c4dff7096164
wuesis <wuesis98@hotmail.com>
2022-07-22 20:50:39 -0500

    Contador fresco terminado.

--- src/Pruebas/Contador.tsx
+++ src/Pruebas/Contador.tsx
@@ -7,11 +7,18 @@ export function Contador() {
 
     useEffect(() => {
         state > 0 ? document.body.style.backgroundColor = "green" : state == 0 ? document.body.style.backgroundColor = "gray" : document.body.style.backgroundColor = "red";
+        document.getElementById("Manual").value = state
     });
 
-    function gg() {
-        const value: number = Number(document.getElementById("Manual").value);
-        return setState(value);
+    function onNumberCapture(e) {
+    
+        if (Number(e.currentTarget.value)) {
+            const value: number = Number(document.getElementById("Manual").value);
+            return setState(value);
+        }
+        else{
+            return false;
+        }
     }
 
 
@@ -28,8 +35,7 @@ export function Contador() {
             </div>
 
             <div className='CounterManual'>
-                <input id='Manual' ></input>
-                <button onClick={gg}>Set</button>
+                <input id='Manual' onKeyUp={onNumberCapture} ></input>
             </div>
         </div>
     </>
